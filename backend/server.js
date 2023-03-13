1f4900fb96ccccf01af48dd32289ca1440cdfbde
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-03-13 14:49:14 +0530

    added cors of sparshsvnit.in

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://sparshsvnit.in/");
+  res.header("Access-Control-Allow-Origin", "https://sparshsvnit.in");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
