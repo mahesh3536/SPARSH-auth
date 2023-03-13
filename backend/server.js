7cd0671db51c3df2cf0fe2fdf4ee3805c3095c7a
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-03-13 14:56:49 +0530

    fixing cors error

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const protect = require("./middleware/protect");
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://sparshsvnit.in");
